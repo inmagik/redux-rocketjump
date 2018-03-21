f6c094e3f22d941f03ccdc212994e32d43294298
gffuma <fumagalli.gf@gmail.com>
2018-03-21 19:42:23 +0100

    Tested rj constructor

---
+++ src/__tests__/rocketjump.js
@@ -0,0 +1,14 @@
+import { rocketjump } from '../core'
+
+describe('Rocketjump', () => {
+  it('should throw exception when no type is provided', () => {
+    expect(rocketjump({
+      state: 'soci',
+    })).toThrow()
+  })
+  it('should throw exception when no state is provided', () => {
+    expect(rocketjump({
+      type: 'GET_SOCI',
+    })).toThrow()
+  })
+})
