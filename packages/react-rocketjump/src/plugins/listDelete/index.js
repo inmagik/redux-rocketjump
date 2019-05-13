36c3b17f411d8c44a28ccad499c2b49406d61835
Osio Alberto <osioalberto@gmail.com>
2019-05-13 18:16:26 +0200

    Testing for ListDelete + bugfixes

--- packages/react-rocketjump/src/plugins/listDelete/index.js
+++ packages/react-rocketjump/src/plugins/listDelete/index.js
@@ -15,14 +15,20 @@ const rjListDelete = (config = {}) => {
     }),
     reducer: oldReducer => (state, action) => {
       if (action.type === TYPE) {
-        if (state.data.pagination && config.warnPagination !== false) {
+        const paginationPath = path
+          .split('.')
+          .map((item, i, arr) => i === arr.length - 1 ? 'pagination' : item)
+          .join('.')
+        if (state && get(state, paginationPath) && config.warnPagination !== false) {
           // eslint-disable-next-line no-console
           console.warn(
             'It seems you are using this plugin on a paginated list. Remember that this plugin is agnostic wrt pagination, and will break it. To suppress this warning, set warnPagination: false in the config object'
           )
         }
         const newState = {...state}
-        set(newState, path, get(newState, path).filter(listItem => !identity(action, listItem)))
+        let list = get(newState, path)
+        if (list) list = list.filter(listItem => !identity(action, listItem))
+        set(newState, path, list)
         return newState
       }
       return oldReducer(state, action)
