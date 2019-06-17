02743182c86f465e917f2dfd09d94a20b5b4d7da
Osio Alberto <osioalberto@gmail.com>
2019-06-17 16:49:07 +0200

    Bad docusaurus config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -28,7 +28,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'redux-rocketjump',
-  organizationName: 'InMagik s.r.l.',
+  organizationName: 'inmagik',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
