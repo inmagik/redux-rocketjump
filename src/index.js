b00ccac67dda7a584c5d3aeaeeb11a4a3e840d20
gffuma <fumagalli.gf@gmail.com>
2020-01-17 14:59:52 +0100

    Export deps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// MagIK DeBps for useRunRj hook
+import * as allDeps from './deps'
+
 export { rj } from './rocketjump'
 export * from './helpers'
 export * from './effects'
@@ -8,3 +11,6 @@ export { default as combineRjs } from './combineRjs'
 export { default as rjMiddleware } from './rjMiddleware'
 export { default as useRj } from './useRj'
 export { default as useRunRj } from './useRunRj'
+
+// Exports DeBps
+export const deps = { ...allDeps } // Remove es6 module shit
