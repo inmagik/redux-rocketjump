1c5737e97235385d2b8aa67b42bddade85047cb5
gffuma <fumagalli.gf@gmail.com>
2020-01-22 11:52:56 +0100

    Strict comparsion in rjMiddleware check the ends of SUCCESS and FAILURE instead of simply contains

--- src/rjMiddleware.js
+++ src/rjMiddleware.js
@@ -12,20 +12,25 @@ export const registerCallbacks = callbacks => {
   ]
 }
 
+const strEndsWith = (str, search) => {
+  const len = str.length
+  return str.substring(len - search.length, len) === search
+}
+
 const rjMiddleware = store => next => action => {
   if (action.meta && callbacksMap[action.meta.rjCallId]) {
     // Current action match a callback id in current hook context
     const rjCallId = action.meta.rjCallId
     const callbacks = callbacksMap[rjCallId]
 
-    if (action.type.indexOf('SUCCESS') !== -1) {
+    if (strEndsWith(action.type, 'SUCCESS')) {
       // On Success
       if (typeof callbacks.onSuccess === 'function') {
         callbacks.onSuccess(action.payload.data)
       }
       callbacks.onComplete()
       delete callbacksMap[rjCallId]
-    } else if (action.type.indexOf('FAILURE') !== -1) {
+    } else if (strEndsWith(action.type, 'FAILURE')) {
       // On Failure
       if (typeof callbacks.onFailure === 'function') {
         callbacks.onFailure(action.payload)
